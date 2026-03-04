8761f69ca0bf055184c615393f7fad21d0d38a6a
VargaAti <varga.attila@diak.szbi-pg.hu>
2026-03-04 08:37:04 +0100

    Gyorsítás, fékezés metódus

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from auto import Auto
 
-auto1 = Auto("Toyota", "Corolla", 2015)
+auto1 = Auto("Toyota", "Corolla", 2015,)
 auto2 = Auto("Ford", "Focus", 2018)
 
 print(auto1)
 print(auto2)
+
+auto1.gyorsit(250)
+print(auto1)
+
+auto1.fekez(50)
+print(auto1)
